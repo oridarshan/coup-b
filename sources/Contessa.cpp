99f2dda04f4c54da5bd32ac6b1b3bf194d890126
Ori Darshan <oridarshan@gmail.com>
2022-05-10 17:50:42 +0300

    Contessa block

--- sources/Contessa.cpp
+++ sources/Contessa.cpp
@@ -2,7 +2,19 @@
 
 namespace coup{
     
-    void Contessa::block(Player& other){}
+    void Contessa::block(Player& other)
+    {
+        if (other.last_action().action != Action::assassinate)
+        {
+            throw ("Contessa can only block 'assassinate'");
+        }
+        if (other.last_action().subject->get_status() == Status::dead)
+        {
+            throw ("not a necromancer, can't revive the dead");
+        }
+        other.last_action().subject->set_status(Status::alive);
+        
+    }
 
     Contessa::Contessa(Game& game, std::string name) : Player(game, name)
     {
